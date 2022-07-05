073532d3a2e2870847e7459a73950a62b34d0cd5
Kenneth Kim <kkm2059@gmail.com>
2022-07-05 17:51:54 -0400

    prepend

--- LinkedList/first.js
+++ LinkedList/first.js
@@ -11,6 +11,13 @@
 //     }
 // }
 
+class Node {
+    constructor(value) {
+        this.value = value,
+        this.next = null
+    }
+}
+
 class LinkedList {
     constructor(value){
         this.head = {
@@ -22,18 +29,25 @@ class LinkedList {
     }
     append(value) {
         // let node = new this.Node(data)
-        const newNode = {
-            value: value,
-            next: null
-        }
+        const newNode = new Node(value)
         this.tail.next = newNode;
         this.tail = newNode
         this.length++
-        return this;
+        // return this;
+    }
+
+    prepend(value) {
+        const newNode = new Node(value)
+        newNode.next = this.head
+        this.head = newNode
+        this.length++
+        return this
     }
 }
 
 const myLinkedList = new LinkedList(10);
 myLinkedList.append(5)
 myLinkedList.append(16)
+myLinkedList.prepend(1)
+// myLinkedList.prepend(1515)
 console.log(myLinkedList)
