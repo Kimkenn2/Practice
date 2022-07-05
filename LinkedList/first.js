b79454b45ffcd33dcf21b59232a387babe53c4c8
Kenneth Kim <kkm2059@gmail.com>
2022-07-05 18:29:04 -0400

    insert and print list

--- LinkedList/first.js
+++ LinkedList/first.js
@@ -43,6 +43,35 @@ class LinkedList {
         this.length++
         return this
     }
+    printList() {
+        const array = [];
+        let currentNode = this.head;
+        while(currentNode !== null){
+            array.push(currentNode.value);
+            currentNode = currentNode.next
+        }
+        console.log(array)
+        // console.log(this.tail.next == null)
+    }
+    insert(index, value){
+        //check params
+        if(index >= this.length){
+            return this.append(value)
+        }
+        const newNode = new Node(value)
+        const leader  = this.traverseToIndex(index-1)
+        newNode.next = leader.next
+        leader.next = newNode
+    }
+    traverseToIndex(index) {
+        let counter = 0;
+        let currentNode = this.head;
+        while(counter !== index){
+            currentNode = currentNode.next
+            counter++
+        }
+        return currentNode
+    }
 }
 
 const myLinkedList = new LinkedList(10);
@@ -50,4 +79,8 @@ myLinkedList.append(5)
 myLinkedList.append(16)
 myLinkedList.prepend(1)
 // myLinkedList.prepend(1515)
-console.log(myLinkedList)
+myLinkedList.insert(2, 99)
+myLinkedList.insert(1, 414)
+myLinkedList.insert(152125, 1)
+myLinkedList.printList()
+// console.log(myLinkedList)
