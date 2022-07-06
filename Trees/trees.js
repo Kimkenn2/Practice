ef60b187fcde3da445288aac0e108a80fb792c19
Kenneth Kim <kkm2059@gmail.com>
2022-07-05 22:57:26 -0400

    finished tree

--- Trees/trees.js
+++ Trees/trees.js
@@ -14,15 +14,68 @@ class BinarySearchTree {
         const newNode = new Node(value);
         if(this.root === null){
             this.root = newNode
+        } else {
+            let currentNode = this.root;
+            while(true) {
+                if(value < currentNode.value){
+                    //left
+                    if(!currentNode.left){
+                        currentNode.left = newNode;
+                        return this
+                    }
+                    currentNode = currentNode.left
+                } else {
+                    //right
+                    if(!currentNode.right){
+                        currentNode.right = newNode;
+                        return this;
+                    } 
+                    currentNode = currentNode.right
+                }
+            }
         }
     }
     lookup(value){
+        // let currentNode = this.root
+        // // console.log(currentNode)
+        // while(currentNode) {
+        //     if(currentNode.value === value){
+        //         return currentNode.value
+        //     } else if (value < currentNode.value){
+        //         currentNode = currentNode.left
+        //     } else if (value > currentNode.value){
+        //         currentNode = currentNode.right
+        //     }
+        //     if(currentNode.right == null && currentNode.left == null){
+        //         return("not found")
+        //     }
+        // }
+        if(!this.root){
+            return false;
+        }
+        let currentNode = this.root;
+        while(currentNode){
+            if(value < currentNode.value){
+                currentNode = currentNode.left
+            } else if (value > currentNode.value){
+                currentNode = currentNode.right;
+            } else if (currentNode.value === value){
+                return currentNode;
+            }
+        }
+        return false
     }
 }
 
 const tree = new BinarySearchTree();
 tree.insert(1)
-console.log(JSON.stringify(traverse(tree.root)))
+tree.insert(2)
+tree.insert(15)
+tree.insert(13)
+tree.insert(0)
+tree.insert(14)
+// console.log(JSON.stringify(traverse(tree.root)))
+console.log(tree.lookup(15))
 
 
 //       9
